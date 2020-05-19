144378fc220bc3b34b4dd685a40c796856d68fdb
Haylie Goh <h.goh@deakin.edu.au>
2020-05-19 20:35:11 +1000

    get route for categories added

--- routes/story-apiRoutes.js
+++ routes/story-apiRoutes.js
@@ -12,6 +12,18 @@ router.get("/api/stories", function (req, res) {
   });
 });
 
+//GET all the stories for EVERYDAY Heroes
+router.get("/api/stories/category", function (req, res) {
+  db.Story.findAll({
+    where: {
+      CategoryId: req.params.id,
+    },
+    include: [db.User],
+  }).then(function (dbStory) {
+    res.json(dbStory);
+  });
+});
+
 //POST for saving a new post
 router.post("/api/stories", function (req, res) {
   db.Story.create(req.body).then(function (dbStory) {
