ed396bb32cc19c93d4485bb8d8a68e9518f6a10e
Haylie Goh <h.goh@deakin.edu.au>
2020-05-20 21:01:31 +1000

    amended

--- routes/user-apiRoutes.js
+++ routes/user-apiRoutes.js
@@ -7,26 +7,21 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 // If the user has valid login credentials, send them to the members page.
 // Otherwise the user will be sent an error
-router.post("/api/login", passport.authenticate("local"), function (req, res) {
+router.post("/login", passport.authenticate("local"), function (req, res) {
   res.json(req.user);
 });
 
 //logging the user in
-router.post("/api/signup", function (req, res) {
+router.post("/signup", function (req, res) {
   db.User.create({
     name: req.body.name,
     password: req.body.password,
   })
     .then(function () {
-      res.redirect(307, "/api/login");
+      res.redirect(307, "/login");
     })
     .catch(function (err) {
-      // res.status(401).json(err);
-      res
-        .status(401)
-        .json(
-          "Please use letters and numbers only when creating your username."
-        );
+      res.status(401).json(err);
     });
 });
 
@@ -36,19 +31,19 @@ router.get("/logout", function (req, res) {
   res.redirect("/");
 });
 
-// GET data about user
-router.get("/api/user_data", function (req, res) {
-  if (req.user) {
-    res.json({
-      name: req.user.name,
-      id: req.user.id,
-    });
-  }
-  res.json({});
-});
+// // GET data about user
+// router.get("/user_data", function (req, res) {
+//   if (req.user) {
+//     res.json({
+//       name: req.user.name,
+//       id: req.user.id,
+//     });
+//   }
+//   res.json({});
+// });
 
 //GET stories for the current user
-router.get("/api/user/stories", isAuthenticated, function (req, res) {
+router.get("/user/stories", isAuthenticated, function (req, res) {
   db.Story.findAll({
     where: {
       UserId: req.params.id,
